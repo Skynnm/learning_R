3797bbc43c990431d4776a7371a42ee2a57b167a
Skynnm <1208909663@qq.com>
2026-06-12 23:58:47 +0800

    Add files via upload

---
+++ 练习20260612：创建一副扑克牌.R
@@ -0,0 +1,27 @@
+# 数据框是列表的二维版本，从数据分析角度来看它无疑是最有用的数据存储结构
+# 数据框将向量组织在一个二维的表格之中，【每一个向量都是这个表格中的一列】
+
+# 练习20260612：创建一副扑克牌
+deck <- data.frame(
+  face = c("king", "queen", "jack", "ten", "nine", "eight", "seven", "six",
+           "five", "four", "three", "two", "ace", "king", "queen", "jack", 
+           "ten", "nine", "eight", "seven", "six", "five", "four", "three", 
+           "two", "ace", "king", "queen", "jack", "ten", "nine", "eight",
+           "seven", "six", "five", "four", "three", "two", "ace", "king",
+           "queen", "jack", "ten", "nine", "eight", "seven", "six", "five",
+           "four", "three", "two", "ace"),
+  suit = c("spades", "spades", "spades", "spades", "spades", "spades","spades",
+           "spades", "spades", "spades", "spades", "spades", "spades", "clubs", 
+           "clubs", "clubs", "clubs", "clubs", "clubs", "clubs", "clubs", "clubs",
+           "clubs", "clubs", "clubs", "clubs", "diamonds", "diamonds", "diamonds",
+           "diamonds", "diamonds", "diamonds", "diamonds", "diamonds", "diamonds",
+           "diamonds", "diamonds", "diamonds", "diamonds", "hearts", "hearts", 
+           "hearts", "hearts", "hearts", "hearts", "hearts", "hearts", "hearts",
+           "hearts", "hearts", "hearts", "hearts"),
+  value = c(13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 13, 12, 11, 10, 9, 8, 7, 6,
+            5, 4, 3, 2, 1, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 13, 12, 11,
+            10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+)
+
+# 错误于data.frame(face = c("king", "queen", "jack", "ten", "nine", "eight", : 
+# 参数值意味着不同的行数: 52, 39, 65
