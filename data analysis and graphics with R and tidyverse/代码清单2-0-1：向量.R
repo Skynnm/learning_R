82016aeee3922d3f8a5619280cd28b960a37c13a
Skynnm <1208909663@qq.com>
2026-07-12 12:04:48 +0800

    Add files via upload

---
+++ data analysis and graphics with R and tidyverse/代码清单2-0-1：向量.R
@@ -0,0 +1,7 @@
+# 向量（vector）是用于存储单一数据类型的一维数组。
+# 执行组合功能的函数c()可用来创建向量。catenate, collect, combine
+
+# 代码清单2-0-1：向量
+a <- c(1, 2, 5, 3, 6, -2, 4)
+b <- c("one", "two", "three")
+c <- c(TRUE, TRUE, TRUE, FALSE, TRUE, FALSE)
