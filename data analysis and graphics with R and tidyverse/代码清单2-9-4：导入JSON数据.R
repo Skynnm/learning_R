ab69e60c21c433c0c6f6ca399d80af41462a285a
Skynnm <1208909663@qq.com>
2026-07-30 20:10:37 +0800

    Add files via upload

---
+++ data analysis and graphics with R and tidyverse/代码清单2-9-4：导入JSON数据.R
@@ -0,0 +1,14 @@
+# JSON：Javascript Object Notation
+
+# 代码清单2-9-4：导入JSON数据
+# 现在，越来越多的数据都以JSON（Javascript Object Notation）格式提供。
+# R中有很多处理JSON数据的包。例如，jsonlite包可以读取、写入和操作JSON文件。
+# 我们可以将数据从JSON文件直接导入R数据框。可以参阅jsonlite包的帮助文档。
+
+# 打开包总览文档（推荐）
+# 查看jsonlite包整体说明、函数列表
+help(package = "jsonlite")
+
+
+# 打开官方网页版帮助
+browseVignettes("jsonlite")
